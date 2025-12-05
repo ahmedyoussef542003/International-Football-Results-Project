c8968d7e071a3bde28008fed7305076c0af35f8f
AhmedY <ahmedyoussefelhewag@gmail.com>
2025-12-05 17:43:02 +0200

    Ahmed Y : I fixed all Errors 2,3,4 queries

--- actual project Internatiomnal .sql
+++ actual project Internatiomnal .sql
@@ -1,59 +1,123 @@
 USE [International_football] 
 GO 
 
--- Which cities have hosted the largest number of international matches?
-SELECT city ,
+
+
+
+
+
+
+
+
+
+
+--- ===========Basic Exploration and Data Cleaning=================
+--Display count of data 
+SELECT count(*) AS NUMOFROWS
+FROM results
+
+
+---Display name of columns 
+SELECT TOP 0 *
+FROM results;
+
+-- checking for missing value
+SELECT * 
+FROM results
+WHERE date IS NULL
+           OR home_team IS NULL
+           OR away_team IS NULL 
+		   OR home_score IS NULL 
+		   OR away_score IS NULL
+		   OR tournament IS NULL
+		   OR city IS NULL
+		   OR country IS NULL
+		   OR neutral IS NULL;
+
+--- checking for duplicates 
+SELECT date, home_team, away_team, home_score, away_score, tournament, city, country, neutral,
+       COUNT(*) AS duplicates_count
+FROM results
+GROUP BY date, home_team, away_team, home_score, away_score, tournament, city, country, neutral
+HAVING COUNT(*) > 1;
+ 
+-----===============================  Goals  =========================
+--1- Which cities have hosted the largest number of international matches?
+SELECT TOP 5 city ,
        COUNT(city) AS MOSTCITY 
 FROM results 
 GROUP BY city
 ORDER BY MOSTCITY DESC ;
 
 
----Do home teams generally have a higher chance of winning?
-
+--2-Do home teams generally have a higher chance of winning?
 SELECT 
-   (
-   SELECT 
-   (
-	  (COUNT(home_team)*100.00) /
-   
-      (SELECT COUNT(home_team) FROM results )
-   
-   ) 
-    FROM results
-    WHERE home_score > away_score )AS HOMETEAMCOUNT 
-   
+    (SUM(CASE WHEN home_score > away_score THEN 1 ELSE 0 END) * 100.0 
+        / COUNT(*)) AS HomeTeamWinPercentage,
+        
+    (SUM(CASE WHEN home_score = away_score THEN 1 ELSE 0 END) * 100.0
+        / COUNT(*)) AS DrawPercentage,
+	(SUM (CASE WHEN  home_score< away_score THEN 1 ELSE 0 END ) * 100.0
+	    /COUNT(*)) AS AwayTeamWinPercentage
+FROM results;
 
 
---Which tournaments have the highest percentage of matches that end in a draw?   
-SELECT( 
-   SELECT (
-        (COUNT(*) * 100.00 ) /
-        (SELECT COUNT(*) FROM results) 
-	      )
-   FROM results
-   WHERE home_score = away_score );
 
----Which teams score the most goals on average, and which teams concede the most goals on average?
-SELECT  home_team , AVG (home_score ) AS  AVG_HOMESCORE_BER_TEAM
+--3- Which tournaments have the highest 
+--percentage of matches that end in a draw?   
+
+SELECT TOP 1 tournament , COUNT (*) AS CountdrawperTournament ,
+     CONCAT(
+   
+        (COUNT(*) * 100 / 
+          (SELECT COUNT(*) FROM results WHERE home_score = away_score)
+        ), 
+          '%'
+      ) AS Percentage
 FROM results
-GROUP BY home_team
-HAVING AVG (home_score) > (SELECT AVG (home_score) FROM results )
-ORDER BY AVG_HOMESCORE_BER_TEAM DESC 
+WHERE home_score = away_score
+GROUP BY tournament
+ORDER BY CountdrawperTournament DESC; 
+
+
+
+--4-Which teams score the most goals on average?
+SELECT TOP 5 team,
+       AVG(goals_scored) AS avg_goals_scored
+FROM (
+        SELECT home_team AS team, home_score AS goals_scored
+        FROM results
+        UNION ALL
+        SELECT away_team AS team, away_score AS goals_scored
+        FROM results
+     ) AS all_goals
+GROUP BY team
+HAVING AVG(goals_scored) > (SELECT AVG(away_score + home_score  )  FROM results)
+ORDER BY avg_goals_scored DESC;
 
 
 
 -- which teams concede the most goals on average?
-SELECT  home_team , AVG (away_score ) AS  Strongest_defensive_team
-FROM results
-GROUP BY home_team
-HAVING AVG (away_score) > (SELECT AVG (away_score) FROM results )
-ORDER BY Strongest_defensive_team DESC     
+SELECT TOP 5 team,
+       AVG(goals_conceded) AS avg_conceded
+FROM (
+        SELECT home_team AS team, away_score AS goals_conceded
+        FROM results
+        
+        UNION ALL
+        
+        SELECT away_team AS team, home_score AS goals_conceded
+        FROM results
+     ) AS all_conceded
+GROUP BY team
+HAVING AVG(goals_conceded) > (SELECT AVG(away_score+home_score) FROM results)
+ORDER BY avg_conceded DESC;
+    
 
 
 -- What are the matches with the highest total number of goals,
 -- and what patterns can be observed from them?
-SELECT home_team ,
+SELECT TOP 5 home_team ,
       away_team ,
 	  tournament ,
 	  city , 
